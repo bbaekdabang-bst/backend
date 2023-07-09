6947753cf38609d9ade517d60ede1256b8358a38
hongminjeong <meanj@hongminjeongui-MacBookAir.local>
2023-07-09 12:03:17 +0900

    first commit

---
+++ src/main/java/com/bbacks/bst/BstApplication.java
@@ -0,0 +1,13 @@
+package com.bbacks.bst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbacks/bst/BstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbacks.bst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
