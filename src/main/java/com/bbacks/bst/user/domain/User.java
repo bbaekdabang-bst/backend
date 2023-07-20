ef3936f3ea88c0e2d14750be44225bc7fc5d867b
nayawgj <nayawgj@gmail.com>
2023-07-20 22:24:40 +0900

    User 엔티티 매핑 수정

--- src/main/java/com/bbacks/bst/user/domain/User.java
+++ src/main/java/com/bbacks/bst/user/domain/User.java
@@ -3,14 +3,14 @@ package com.bbacks.bst.user.domain;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "user_info")
+@Table(name = "user")
 public class User {
 
     @Id @GeneratedValue
     @Column(name = "u_id")
     private int userId;
 
-    @Column(name = "u_nickname")
+    @Column(name = "u_nickname", unique = true, length = 20)
     private String userNickname;
 
     @Column(name = "u_social_id")
