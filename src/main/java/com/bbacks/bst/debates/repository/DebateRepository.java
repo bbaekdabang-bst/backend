0679624ae4e266c4b8b6b4235ea26f0bff2aa0bd
dawoon08 <lusia08291@gmail.com>
2023-08-14 11:22:49 +0900

    FEAT: 메인 화면 구현

--- src/main/java/com/bbacks/bst/debates/repository/DebateRepository.java
+++ src/main/java/com/bbacks/bst/debates/repository/DebateRepository.java
@@ -8,12 +8,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DebateRepository extends JpaRepository<Debate, Long> {
     @Modifying
     @Query(value = "UPDATE Debate d SET d.debateParticipants = d.debateParticipants + 1 WHERE d.debateId = :debateId")
     int incrementCountByDebateId(@Param("debateId") Long debateId);
 
-
+    List<Debate> findTop10ByOrderByDebateParticipants();
+    List<Debate> findTop10ByOrderByDebateCreatedAtDesc();
 
 }
