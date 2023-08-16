976c5d4cfdefa56ff8c9723b918b3719f6c3603f
hongminjeong <meanj@hongminjeongui-MacBookAir.local>
2023-08-16 11:59:54 +0900

    REFACTOR: null 비교 리팩토링

--- src/main/java/com/bbacks/bst/global/response/SuccessStatus.java
+++ src/main/java/com/bbacks/bst/global/response/SuccessStatus.java
@@ -21,8 +21,9 @@ public enum SuccessStatus {
     LIKE_POST_SUCCESS(HttpStatus.OK, "좋아요에 성공하였습니다."),
     UNLIKE_POST_SUCCESS(HttpStatus.OK, "좋아요를 취소하였습니다."),
     DISLIKE_POST_SUCCESS(HttpStatus.OK, "싫어요에 성공하였습니다."),
-    UNDISLIKE_POST_SUCCESS(HttpStatus.OK, "싫어요를 취소하였습니다.");
+    UNDISLIKE_POST_SUCCESS(HttpStatus.OK, "싫어요를 취소하였습니다."),
 
+    NO_CONTENT(HttpStatus.NO_CONTENT, "조회된 데이터가 없습니다.");
 
 
     private final HttpStatus httpStatus;
